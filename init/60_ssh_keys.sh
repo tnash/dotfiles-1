6589b805b88f9e9c73e4a14157a040c56f81970b
Stephen Roberts <stephenroberts@gmail.com>
2015-03-10 17:42:38 -0500

    Symlink with Dropbox files, decrypt ssh keys from Dropbox, and various fixes

---
+++ init/60_ssh_keys.sh
@@ -0,0 +1,25 @@
+# OSX-only stuff. Abort if not OSX.
+#is_osx || return 1
+
+KEY_DIR=~/Dropbox/SSH\ Keys
+
+# Check if keys are available
+[ -e "$KEY_DIR" ] || exit 1
+
+WD=$(pwd)
+
+keys=$(ls "$KEY_DIR" | grep enc$)
+for k in $keys
+do
+
+	# Skip if key exists
+	[ -e ~/.ssh/${k%.enc} ] && continue
+
+	# Copy key and decrypt
+	cp "$KEY_DIR/$k" ~/.ssh/ && cd ~/.ssh && decrypt $k
+done
+
+# Clean up
+rm ~/.ssh/*.enc
+
+cd $WD
