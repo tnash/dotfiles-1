e2b2bd8e73906784f6805ed394af77847c3178f5
Stephen Roberts <stephenroberts@gmail.com>
2018-08-23 21:18:38 -0600

    feat: use pip3

--- init/60_osx_misc.sh
+++ init/60_osx_misc.sh
@@ -1,7 +1,7 @@
 # OSX-only stuff. Abort if not OSX.
 is_osx || return 1
 
-if [[ "$(type -p pip)" ]]; then
-  pip install --user powerline-status
-  pip install psutil
+if [[ "$(type -p pip3)" ]]; then
+  pip3 install --user powerline-status
+  pip3 install psutil
 fi
