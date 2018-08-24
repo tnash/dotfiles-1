af4bf6673d78869f686e77639cce475f6c0d6d9c
Stephen Roberts <stephenroberts@gmail.com>
2018-08-23 18:30:49 -0600

    feat(zsh): install powerlevel9k

--- init/50_zsh.sh
+++ init/50_zsh.sh
@@ -1,4 +1,10 @@
 if [ ! -e "$HOME/.oh-my-zsh" ]; then
+  # Install oh-my-zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  # Re-symlink .zshrc because the installer overwrites it
   ln -sf $DOTFILES/link/.zshrc $HOME/.zshrc
+
+  # Install powerlevel9k
+  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 fi
